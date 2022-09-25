9ab78eea2e5aad85ad16c7aba7d119bfb567e173
VIO <75546139+violettpv@users.noreply.github.com>
2022-09-25 19:43:45 +0300

    scss updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const nameSpan = document.getElementById('fullname-error');
 const nameInput = document.getElementById('cardholder');
 
+// let checkName = /^[A-Z][a-z]+\s[A-Z][a-z]+$/gm;
 // document.getElementById("content").getElementsByTagName("span")[0].innerHTML="";
 
 function sendCardDetails() {
