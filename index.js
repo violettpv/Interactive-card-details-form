afbe5a96376f8d367765750e24db1e9defe6f6e5
VIO <75546139+violettpv@users.noreply.github.com>
2022-09-25 18:36:42 +0300

    css, responsive

---
+++ index.js
@@ -0,0 +1,10 @@
+// === Contact Form
+const nameSpan = document.getElementById('fullname-error');
+const nameInput = document.getElementById('cardholder');
+
+// document.getElementById("content").getElementsByTagName("span")[0].innerHTML="";
+
+function sendCardDetails() {
+
+
+}
